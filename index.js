f10733dd857270aaa0bade6857552225320c495d
PakStar92 <gtech.apiz@gmail.com>
2025-07-27 05:24:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PhotoOxyEnhanced = require('./photooxy-enhanced');
 
 async function testPhotoOxy() {
     const photoOxy = new PhotoOxyEnhanced('https://photooxy.com/logo-and-text-effects/shadow-text-effect-in-the-sky-394.html');
-    photoOxy.setText(['Hello World']);
+    photoOxy.setText(['Hello']);
     
     try {
         const result = await photoOxy.execute();
