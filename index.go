956a87b2f866d914d4314ccf96b46a664bed6da0
PakStar92 <gtech.apiz@gmail.com>
2025-07-27 20:51:56 +0500

    Update and rename index.js to index.go

---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"yourmodule/downloader"
+)
+
+func main() {
+	streamURL, err := downloader.GetStreamURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "mp4")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Stream URL:", streamURL)
+}
